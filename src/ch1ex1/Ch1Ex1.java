e46d5e3913790c59c37667c25ae67f0de00db86d
Sergey Mokhov <s.v.mokhov@gmail.com>
2018-08-07 13:20:20 +0300

    Initial commit

---
+++ src/ch1ex1/Ch1Ex1.java
@@ -0,0 +1,7 @@
+package ch1ex1;
+
+public class Ch1Ex1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ch2ex2/Ch2Ex2.java
@@ -0,0 +1,7 @@
+package ch2ex2;
+
+public class Ch2Ex2 {
+    public static void main(String[] args) {
+
+    }
+}
